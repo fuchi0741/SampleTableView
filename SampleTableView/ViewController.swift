0ad79a56c57fc6d97c903745d5365a3040b410b5
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-09-05 17:24:21 +0900

    初期設定

--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
+    
+    //Cellを何個必要なのか？
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 //Cellが3こ必要
+    }
+    
+    //どんなCellが必要なのか？
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
 }
-
