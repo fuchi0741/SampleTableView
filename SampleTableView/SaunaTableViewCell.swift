6863d35cb4fbe0d2b2f56aa3907493af6f087dd9
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-09-08 21:34:35 +0900

    f

---
+++ SampleTableView/SaunaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SaunaTableViewCell.swift
+//  SampleTableView
+//
+//  Created by 渕一真 on 2021/09/08.
+//
+
+import UIKit
+
+class SaunaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SampleTableView/SaunaViewController.swift
+++ SampleTableView/SaunaViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SaunaViewController.swift
 //  SampleTableView
 //
 //  Created by 渕一真 on 2021/08/29.
@@ -7,21 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class SaunaViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        let nib = UINib(nibName: "SaunaTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Cell")
     }
     
-    //Cellを何個必要なのか？
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5 //Cellが3こ必要
+        return 5
     }
     
-    //どんなCellが必要なのか？
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         return cell
